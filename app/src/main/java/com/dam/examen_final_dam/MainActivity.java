47af21543ee8f3f1861f4834169524de7632c497
Angela Centeno <2017120004@untels.edu.pe>
2021-02-11 15:07:34 -0500

    Funcionabilidad de boton Login

--- app/src/main/java/com/dam/examen_final_dam/MainActivity.java
+++ app/src/main/java/com/dam/examen_final_dam/MainActivity.java
@@ -2,13 +2,25 @@ package com.dam.examen_final_dam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.button);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent( MainActivity.this, formularioUbicacion.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
