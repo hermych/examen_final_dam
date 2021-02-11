7ae8a8145d6ef0182276558a94fc457d69d11f6c
ShadowEcux <josephc_fc@hotmail.com>
2021-02-11 16:28:56 -0500

    Cambios 2- Collantes Morales- estudiante 4

--- app/src/main/java/com/dam/examen_final_dam/MainActivity.java
+++ app/src/main/java/com/dam/examen_final_dam/MainActivity.java
@@ -10,25 +10,16 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button btnLogin;
-    private EditText editTextTextPersonName2, editTextTextPersonName3, editTextTextPersonName4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        editTextTextPersonName2 = (EditText)findViewById(R.id.editTextTextPersonName2);
-        editTextTextPersonName3 = (EditText)findViewById(R.id.editTextTextPersonName3);
-        editTextTextPersonName4 = (EditText)findViewById(R.id.editTextTextPersonName4);
-
 
         btnLogin = findViewById(R.id.button);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent( MainActivity.this, formularioUbicacion.class);
-                i.putExtra("longitud",editTextTextPersonName2.getText().toString());
-                i.putExtra("latitud",editTextTextPersonName3.getText());
-                i.putExtra("marcador",editTextTextPersonName4.getText());
                 startActivity(i);
 
             }
--- app/src/main/java/com/dam/examen_final_dam/formularioUbicacion.java
+++ app/src/main/java/com/dam/examen_final_dam/formularioUbicacion.java
@@ -6,18 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class formularioUbicacion extends AppCompatActivity {
     Button btnUbicacion;
+    private EditText editTextTextPersonName2, editTextTextPersonName3, editTextTextPersonName4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_ubicacion);
+        editTextTextPersonName2 = (EditText)findViewById(R.id.editTextTextPersonName2);
+        editTextTextPersonName3 = (EditText)findViewById(R.id.editTextTextPersonName3);
+        editTextTextPersonName4 = (EditText)findViewById(R.id.editTextTextPersonName4);
         btnUbicacion = findViewById(R.id.buttonUbicacion);
         btnUbicacion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent( formularioUbicacion.this, ubicacionFinal.class);
+                i.putExtra("longitud",editTextTextPersonName2.getText().toString());
+                i.putExtra("latitud",editTextTextPersonName3.getText());
+                i.putExtra("marcador",editTextTextPersonName4.getText());
                 startActivity(i);
 
             }
