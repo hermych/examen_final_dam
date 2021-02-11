7b3867733e46227aa8454b0229402b6c037b4e13
Angela Centeno <2017120004@untels.edu.pe>
2021-02-11 15:33:07 -0500

    Funcionabilidad btnUbicacion - Estudiante 03

--- app/src/main/java/com/dam/examen_final_dam/formularioUbicacion.java
+++ app/src/main/java/com/dam/examen_final_dam/formularioUbicacion.java
@@ -2,13 +2,25 @@ package com.dam.examen_final_dam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class formularioUbicacion extends AppCompatActivity {
-
+    Button btnUbicacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_ubicacion);
+        btnUbicacion = findViewById(R.id.buttonUbicacion);
+        btnUbicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent( formularioUbicacion.this, ubicacionFinal.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
